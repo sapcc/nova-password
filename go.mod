faf67dd28c27907d9c4ef3a302802cca10293e87
kayrus <kay.diam@gmail.com>
2019-10-20 23:00:03 +0200

    Update gophercloud dependencies

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.12
 
 require (
 	github.com/google/uuid v1.1.1
-	github.com/gophercloud/gophercloud v0.0.0-20190516165734-b3a23cc94cc5
+	github.com/gophercloud/gophercloud v0.6.1-0.20191019020556-0907b320e0ac
+	github.com/gophercloud/utils v0.0.0-20191020172814-bd86af96d544
 	github.com/howeyc/gopass v0.0.0-20170109162249-bf9dde6d0d2c
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/kayrus/putty v0.0.0-20190531171731-550ff2bc782c
--- main.go
+++ main.go
@@ -11,9 +11,10 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/gophercloud/gophercloud"
-	"github.com/gophercloud/gophercloud/acceptance/clients"
 	"github.com/gophercloud/gophercloud/openstack"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/servers"
+	"github.com/gophercloud/utils/client"
+	"github.com/gophercloud/utils/openstack/clientconfig"
 	"github.com/howeyc/gopass"
 	"github.com/kayrus/putty"
 	env "github.com/sapcc/cloud-env"
@@ -170,31 +171,32 @@ func initRootCmdFlags() {
 // newComputeV2 creates a ServiceClient that may be used with the v2 compute
 // package.
 func newComputeV2() (*gophercloud.ServiceClient, error) {
-	ao, err := env.AuthOptionsFromEnv()
+	ao, err := clientconfig.AuthOptions(nil)
 	if err != nil {
 		return nil, err
 	}
 
-	client, err := openstack.NewClient(ao.IdentityEndpoint)
+	provider, err := openstack.NewClient(ao.IdentityEndpoint)
 	if err != nil {
 		return nil, err
 	}
 
 	if viper.GetBool("debug") {
-		client.HTTPClient = http.Client{
-			Transport: &clients.LogRoundTripper{
-				Rt: &http.Transport{},
+		provider.HTTPClient = http.Client{
+			Transport: &client.RoundTripper{
+				Rt:     &http.Transport{},
+				Logger: &client.DefaultLogger{},
 			},
 		}
 	}
 
-	err = openstack.Authenticate(client, ao)
+	err = openstack.Authenticate(provider, *ao)
 	if err != nil {
 		return nil, err
 	}
 
-	return openstack.NewComputeV2(client, gophercloud.EndpointOpts{
-		Region: env.Get("OS_REGION_NAME"),
+	return openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
+		Region: os.Getenv("OS_REGION_NAME"),
 	})
 }
 
